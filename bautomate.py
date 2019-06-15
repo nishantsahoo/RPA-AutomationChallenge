0c5b8ddec401a370a4c92ef3fad24356cc331fbe
Nishant Sahoo <nishantsahoo.in@gmail.com>
2019-06-15 19:40:28 +0530

    Final update

--- bautomate.py
+++ bautomate.py
@@ -7,34 +7,16 @@ sheet = book.active
 
 # create a new Firefox session
 driver = webdriver.Firefox()
-driver.implicitly_wait(30)
 driver.maximize_window()
 
-# Navigate to the application home page
 driver.get("http://rpachallenge.com/")
 
-javascript_exec = ""
-
-# first click on go button
+javascript_exec = "$('button#start').click();"
 
 for i in range(1,11):
 	for col in sheet.iter_cols(max_col=7, max_row=11, values_only=True):
-		print(col[0], col[i])
-		# javascript_exec += "$(\"label:contains(\"" + str(col[0]).strip() + "\")\").parentElement.lastElementChild.value = " + str(col[i]).strip() + ";"
-		
-		# javascript_exec = "console.log($(\"label:contains(\"First Name\")\"));";
+		javascript_exec += "$(\"label:contains(\\\"" + str(col[0]).strip() + "\\\")\")[0].parentElement.lastElementChild.value=\"" + str(col[i]).strip() + "\";"		
 
-	# print(javascript_exec)
-	# driver.execute_script(javascript_exec);	
+	javascript_exec += "$('input.btn').click();"
 
-# javascript_exec = "ele=$(\"label:contains(\\\"First Name\\\")\");console.log(ele);"
-# print(javascript_exec)
-javascript_exec += "$(\"label:contains(\\\"" + "First Name" + "\\\")\")[0].parentElement.lastElementChild.value = " + "\"John\"" + ";"
-print(javascript_exec)
 driver.execute_script(javascript_exec)
-
-# javaScript = "console.log(789);"
-# driver.execute_script(javaScript)
-
-# Use var element = $("label:contains('SuperSweetCheckbox')"); to find label whose inner HTML value is SuperSweetCheckbox
-# $("label:contains('col_val')").parent.children -> input = row['col_val']
