3684b74f4d53945e53ad01c30682be0d4b17a2c7
Nishant Sahoo <nishantsahoo.in@gmail.com>
2019-06-15 14:46:56 +0530

    Parsing the file in a way the website can be autopopulated

--- bautomate.py
+++ bautomate.py
@@ -13,8 +13,13 @@ sheet = book.active
 # # Navigate to the application home page
 # driver.get("http://rpachallenge.com/")
 
-for row in sheet.iter_rows(max_col=7, max_row=11, values_only=True):
-	print(row)
+for i in range(1,11):
+	for col in sheet.iter_cols(max_col=7, max_row=11, values_only=True):
+		print(col[0], col[i])
+		# find all input fields by col[0] and set input field value = col[i]
 
 # javaScript = "console.log(789);"
 # driver.execute_script(javaScript)
+
+# Use var element = $("label:contains('SuperSweetCheckbox')"); to find label whose inner HTML value is SuperSweetCheckbox
+# $("label:contains('col_val')").parent.children -> input = row['col_val']
